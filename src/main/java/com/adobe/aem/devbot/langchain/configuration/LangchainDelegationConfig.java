ee742f5e974cacd1961089bc5f3ee3e273a649bc
cioriia <cioriia@adobe.com>
2024-07-03 12:00:45 +0300

    Use the proper ollama API.

--- src/main/java/com/adobe/aem/devbot/langchain/configuration/LangchainDelegationConfig.java
+++ src/main/java/com/adobe/aem/devbot/langchain/configuration/LangchainDelegationConfig.java
@@ -1,29 +1,26 @@
 package com.adobe.aem.devbot.langchain.configuration;
 
+import com.adobe.aem.devbot.langchain.service.SpecializedBotService;
 import dev.langchain4j.agent.tool.Tool;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.Map;
-
 @Service
 public class LangchainDelegationConfig {
 
-    private final SpecializedBotsConfiguration specializedBotsConfiguration;
-    private final RestTemplate restTemplate;
+    private final SpecializedBotService specializedBotService;
 
-    public LangchainDelegationConfig(SpecializedBotsConfiguration specializedBotsConfiguration, RestTemplate restTemplate)  {
-        this.specializedBotsConfiguration = specializedBotsConfiguration;
-        this.restTemplate = restTemplate;
+    public LangchainDelegationConfig(SpecializedBotService specializedBotService)  {
+        this.specializedBotService = specializedBotService;
     }
 
     @Tool("Answers a question about JCR code.")
     public String getAnswerForJcrCode(String question) {
-        return restTemplate.getForObject(specializedBotsConfiguration.getJcrBotUrl(), String.class, question);
+        return specializedBotService.getAnswerForJcrCode(question);
     }
 
     @Tool("Answers a question about Sling code.")
     public String getAnswerForSlingCode(String question) {
-        return restTemplate.getForObject(specializedBotsConfiguration.getSlingBotUrl(), String.class, question);
+        return specializedBotService.getAnswerForSlingCode(question);
     }
 }
---
+++ src/main/java/com/adobe/aem/devbot/langchain/service/SpecializedBotService.java
@@ -0,0 +1,32 @@
+package com.adobe.aem.devbot.langchain.service;
+
+import com.adobe.aem.devbot.langchain.configuration.SpecializedBotsConfiguration;
+import org.jetbrains.annotations.Nullable;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+public class SpecializedBotService {
+    public static final String BOT_MODEL = "codellama:7b";
+    private final SpecializedBotsConfiguration specializedBotsConfiguration;
+    private final RestTemplate restTemplate;
+
+    public SpecializedBotService(SpecializedBotsConfiguration specializedBotsConfiguration, RestTemplate restTemplate) {
+        this.specializedBotsConfiguration = specializedBotsConfiguration;
+        this.restTemplate = restTemplate;
+    }
+
+    public String getAnswerForJcrCode(String question) {
+        return getAnswer(question, specializedBotsConfiguration.getJcrBotUrl());
+    }
+
+    public String getAnswerForSlingCode(String question) {
+        return getAnswer(question, specializedBotsConfiguration.getSlingBotUrl());
+    }
+
+    private @Nullable String getAnswer(String question, String botUrl) {
+        return restTemplate.postForObject(botUrl, new BotRequest(question, BOT_MODEL, false), String.class);
+    }
+
+    private record BotRequest(String prompt, String model, boolean stream) {}
+}
