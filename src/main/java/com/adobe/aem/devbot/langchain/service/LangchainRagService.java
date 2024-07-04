90c0d1a92b3f6b0621351d89b395e9068e3010c0
Radu-Andrei Dumitru <radud@adobe.com>
2024-07-04 15:16:52 +0300

    Fix langchain rag service

--- src/main/java/com/adobe/aem/devbot/langchain/service/LangchainRagService.java
+++ src/main/java/com/adobe/aem/devbot/langchain/service/LangchainRagService.java
@@ -19,7 +19,7 @@ import java.util.List;
 
 @Service
 public class LangchainRagService {
-    @Value("classpath:/data_all/marker.json")
+    @Value("classpath:/data/marker.json")
     private Resource testResource;
 
     private final ChatLanguageModel chatLanguageModel;
