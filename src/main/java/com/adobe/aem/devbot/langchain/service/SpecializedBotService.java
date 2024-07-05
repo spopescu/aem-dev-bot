9631c16e450d76f7dee5d9e07b6ad9563ee5dd23
Radu-Andrei Dumitru <radud@adobe.com>
2024-07-05 12:18:56 +0300

    Logging for testing function calling

--- src/main/java/com/adobe/aem/devbot/langchain/service/SpecializedBotService.java
+++ src/main/java/com/adobe/aem/devbot/langchain/service/SpecializedBotService.java
@@ -5,10 +5,14 @@ import org.jetbrains.annotations.Nullable;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.logging.Logger;
+
 @Service
 public class SpecializedBotService {
     private final SpecializedBotsConfiguration specializedBotsConfiguration;
     private final RestTemplate restTemplate;
+
+    Logger logger = Logger.getLogger(SpecializedBotService.class.getName());
 
     public SpecializedBotService(SpecializedBotsConfiguration specializedBotsConfiguration, RestTemplate restTemplate) {
         this.specializedBotsConfiguration = specializedBotsConfiguration;
@@ -16,11 +20,17 @@ public class SpecializedBotService {
     }
 
     public String getAnswerForOakCode(String question) {
+        logger.info("Calling oak bot for answer to question: "  + question);
+        String oakBotAnswer = getAnswer(question, specializedBotsConfiguration.getOakBotUrl());
+        logger.info("Oak bot answer: " + oakBotAnswer);
         return getAnswer(question, specializedBotsConfiguration.getOakBotUrl());
     }
 
     public String getAnswerForSlingCode(String question) {
-        return getAnswer(question, specializedBotsConfiguration.getSlingBotUrl());
+        logger.info("Calling sling bot for answer to question: " + question);
+        String slingBotAnswer = getAnswer(question, specializedBotsConfiguration.getSlingBotUrl());
+        logger.info("Sling bot answer: " + slingBotAnswer);
+        return slingBotAnswer;
     }
 
     private @Nullable String getAnswer(String question, String botUrl) {
